3325c67250cb4b2578e53a494e82d41d3d104013
Setareh Nouri <smouri3@my.bcit.ca>
2022-03-11 12:32:01 -0800

    done

--- scripts/script.js
+++ scripts/script.js
@@ -40,7 +40,7 @@ function videos_4() {
 }  
 
 
-//annotated 
+// //annotated 
 function display_video(){
     // console.log("inside the function")
     //get into the right collection
@@ -55,8 +55,64 @@ function display_video(){
     })
 }
 
+function writesports() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var eventsRef = db.collection("upcoming_events");
 
+    eventsRef.add({
+        Sport: "Downhill Ski",
+        Date: "February 7",
+        city: "Cypress",
+        province: "BC",
+        length: "3 hours",
+        details: 'Downhill is a form of alpine skiing competition. Whereas the other alpine skiing events emphasize turning and technique, downhill emphasizes "the six components of technique, courage, speed, risk, physical condition and judgement", according to the FIS "International Ski Competition Rules".'
+    });
+    eventsRef.add({
+        Sport: "Figure Skating",
+        Date: "February 12",
+        city: "Richmond",
+        province: "BC",
+        length: "6 hours",
+        details: "Figure skating is a sport in which individuals, pairs, or groups perform on figure skates on ice. It was the first winter sport to be included in the Olympic Games, when contested at the 1908 Olympics in London."
+    });
+    eventsRef.add({
+        Sport: "Snow Boarding",
+        Date: "February 17",
+        city: "Cypress",
+        province: "BC",
+        length: "4 hours",
+        details: "Snowboarding is a recreational and competitive activity that involves descending a snow-covered slope while standing on a snowboard that is almost always attached to a rider's feet. It features in the Winter Olympic Games and Winter Paralympic Games. "
+    });
+}
 
+function displayCards(collection) {
+    
+    let cardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var date = doc.data().Date;   // get value of the "name" key
+                var sport = doc.data().Sport;
+                var detail = doc.data().details;
+                let newcard = cardTemplate.content.cloneNode(true);
+                newcard.querySelector('.card-title').innerHTML = sport + " taking place on " + date;
+                newcard.querySelector('.card-location').innerHTML = date;
+                newcard.querySelector('.difficulty').innerHTML = sport;
+                newcard.querySelector('.length').innerHTML = detail;
+                newcard.querySelector('.card-image').src = "./images/" + sport + ".jpeg"; //hikes.jpg
+                // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+
+        })
+}
 
 
 function setup() {
@@ -76,9 +132,9 @@ function setup() {
     // $(".sports").click(function (){
     //     videos(this.id);
     // })
-    display_video();
+    // display_video();
+    displayCards("upcoming_events");
 }
 
+
 jQuery(document).ready(setup);
-
-
