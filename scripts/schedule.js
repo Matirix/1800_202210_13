f3551205f6b094c3a9dc16068898b4d706932164
Setareh Nouri <smouri3@my.bcit.ca>
2022-03-21 22:01:19 -0700

    updated calender

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,32 +1,121 @@
-function writesports() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var eventsRef = db.collection("upcoming_events");
 
-    eventsRef.add({
-        Sport: "Downhill Ski",
-        Date: "February 7",
-        city: "Cypress",
-        province: "BC",
-        length: "3 hours",
-        details: 'Downhill is a form of alpine skiing competition. Whereas the other alpine skiing events emphasize turning and technique, downhill emphasizes "the six components of technique, courage, speed, risk, physical condition and judgement", according to the FIS "International Ski Competition Rules".'
-    });
-    eventsRef.add({
-        Sport: "Figuer Skating",
-        Date: "February 12",
-        city: "Richmond",
-        province: "BC",
-        length: "6 hours",
-        details: "Figure skating is a sport in which individuals, pairs, or groups perform on figure skates on ice. It was the first winter sport to be included in the Olympic Games, when contested at the 1908 Olympics in London."
-    });
-    eventsRef.add({
-        Sport: "Snow Boarding",
-        Date: "February 17",
-        city: "Cypress",
-        province: "BC",
-        length: "4 hours",
-        details: "Snowboarding is a recreational and competitive activity that involves descending a snow-covered slope while standing on a snowboard that is almost always attached to a rider's feet. It features in the Winter Olympic Games and Winter Paralympic Games. "
-    });
+const date = new Date();
+
+const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
+
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
+
+  let days = "";
+
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+  }
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
+      days += `<div>${i}</div>`;
+    }
+  }
+
+  if(nextDays > 0) {
+    for (let j = 1; j <= nextDays; j++) {
+        days += `<div class="next-date">${j}</div>`;
+        monthDays.innerHTML = days;
+    }
 }
+else {
+    monthDays.innerHTML = days;
+}
+};
+
+document.querySelector(".prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+});
+
+renderCalendar();
+
+// function writesports() {
+//     //define a variable for the collection you want to create in Firestore to populate data
+//     var eventsRef = db.collection("upcoming_events");
+
+//     eventsRef.add({
+//         Sport: "Downhill Ski",
+//         Date: "February 7",
+//         city: "Cypress",
+//         province: "BC",
+//         length: "3 hours",
+//         details: 'Downhill is a form of alpine skiing competition. Whereas the other alpine skiing events emphasize turning and technique, downhill emphasizes "the six components of technique, courage, speed, risk, physical condition and judgement", according to the FIS "International Ski Competition Rules".'
+//     });
+//     eventsRef.add({
+//         Sport: "Figuer Skating",
+//         Date: "February 12",
+//         city: "Richmond",
+//         province: "BC",
+//         length: "6 hours",
+//         details: "Figure skating is a sport in which individuals, pairs, or groups perform on figure skates on ice. It was the first winter sport to be included in the Olympic Games, when contested at the 1908 Olympics in London."
+//     });
+//     eventsRef.add({
+//         Sport: "Snow Boarding",
+//         Date: "February 17",
+//         city: "Cypress",
+//         province: "BC",
+//         length: "4 hours",
+//         details: "Snowboarding is a recreational and competitive activity that involves descending a snow-covered slope while standing on a snowboard that is almost always attached to a rider's feet. It features in the Winter Olympic Games and Winter Paralympic Games. "
+//     });
+// }
 // writesports();
 // function displayCards(collection) {
 //     let cardTemplate = document.getElementById("hikeCardTemplate");
